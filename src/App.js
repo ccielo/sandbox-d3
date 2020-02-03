6118ab1d23c921d7da4cf2fbf696f58566f4a8ac
Cielo <buen_cielo@naver.com>
2020-02-04 00:09:44 +0900

    example - d3 with styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from "react";
+import { select, line, curveLinear } from "d3";
+import styled from "styled-components";
+
+const Wrapper = styled.svg`
+  background-color: grey;
+`;
+
 
 function App() {
-  return (
-    <div>
+  const svgRef = useRef();
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
 
-    </div>
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    const Line = line()
+      .x((value, index) => index * 50)
+      .y(value => 150 - value)
+      .curve(curveLinear);
+
+    svg
+      .selectAll("path")
+      .data([data])
+      .join("path")
+      .attr("d", value => Line(value))
+      .attr("fill", "none")
+      .attr("stroke", "blue");
+
+  }, [data]);
+
+  return (
+    <>
+      <Wrapper ref={svgRef} />
+      <button onClick={() => setData(data.map(value => value + 5))}>
+        update data
+      </button>
+      <button onClick={() => setData(data.filter(value => value < 35))}>
+        filter data
+      </button>
+    </>
   );
 }
 
